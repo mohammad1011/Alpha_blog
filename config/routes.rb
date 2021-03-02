e921b79170d31af93fd823a6e51a8094b582be85
mohd <shafiqmohammad56@yahoo.com>
2021-03-02 10:28:03 +0200

    migration files and added articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
-Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 #  root 'application#hellomethod'
-root 'pages#home'
-get 'about', to:'pages#about'
+Rails.application.routes.draw do
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles, only: [:show]
 end
