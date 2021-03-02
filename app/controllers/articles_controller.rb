88fd611933e593e408d33a9a6c3bcc76d80b3c11
mohd <shafiqmohammad56@yahoo.com>
2021-03-02 14:10:00 +0200

    create new page for create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,8 +9,23 @@ class ArticlesController < ApplicationController
 
   def index
 
-      @articles = Article.all
+      @article = Article.new
 
+  end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title , :description))
+
+    if @article.save
+     flash[:notice] = "Article was created successfully."
+     redirect_to @article
+   else
+     render 'new'
+   end
   end
 
 end
