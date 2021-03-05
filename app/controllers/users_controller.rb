ddb64687ddd13850fbf6f47abb189e2a5922cdcc
mohd <shafiqmohammad56@yahoo.com>
2021-03-05 17:06:17 +0200

    ligin and log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,37 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
   end
 
+def index
+  @users = User.paginate(page: params[:page], per_page: 5)
+end
+
+  def show
+
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def edit
+
+end
+
+def update
+
+  if @user.update(user_params)
+    flash[:notice] = "Your account information was successfully updated"
+    redirect_to @user
+  else
+    render 'edit'
+  end
+end
+
   def create
     @user = User.new(user_params)
     if @user.save
+          session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
@@ -14,9 +39,12 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
 end
