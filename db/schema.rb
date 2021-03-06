5b1294220dcfe059609558bbd1c6c57915fb4fa9
mohd <shafiqmohammad56@yahoo.com>
2021-03-06 22:55:05 +0200

    last update on many-mant relation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_181210) do
+ActiveRecord::Schema.define(version: 2021_03_06_125042) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.datetime "updated_at", precision: 6, null: false
